97061b9001dd6e192cc2e5b3d6cf3e78d84162d1
sirojiddin043 <151430587+sirojiddin043@users.noreply.github.com>
2023-12-04 12:25:24 +0500

    нуы

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-var x = 10;
-var y = 3;
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
+//var x = 10;
+//var y = 3;
+//console.log(x + y);
+//console.log(x - y);
+//console.log(x * y);
+//console.log(x / y);
+
+//var x = 'salom';
+//var y = 'proweb';
+
+//console.log(x + y);
+
+
+//var admin = alert(name);
+
+//var name = 'веселий';
+
+//var js = 1000 + "108";
+//console.log(js)
+
+
+//var x = +prompt(10);
+//var y = +prompt(15);
+//var x = -prompt(10);
+//var y = -prompt(15);
+//var x = +prompt(10);
+//var y = -prompt(15);
+
+
+
+//alert(x + y)
+
+
+var x = +prompt(15);
+var y = +prompt(15);
+var z = +prompt(15);
+var res = (x / y / z);
+alert(res)
+
+
