3151dc6e68d9918a335456a2cc01ef5e13e783db
sirojiddin043 <151430587+sirojiddin043@users.noreply.github.com>
2023-12-09 00:03:40 +0500

    ыыы

--- script.js
+++ script.js
@@ -38,18 +38,41 @@
 //alert(res)
 
 
-var x = +prompt('yozing birinchi raqami 1');
-var y = +prompt('yozing ikkinchi raqami 1');
+//var x = +prompt('yozing birinchi raqami 1');
+//var y = +prompt('yozing ikkinchi raqami 1');
 
 
-if (x > y){
-    alert('birinchi raqam 1 kotta');
+//if (x > y){
+//    alert('birinchi raqam 1 kotta');
+//}
+
+//else if(x < y){
+//    alert('ikkinchi raqam 2 kotta');
+//}
+//else{
+//    alert('raqamlar barvar');
+//}
+
+
+//var x = 10;
+//var y = 20;
+//
+//function num() {
+//    if(x > y){
+//        console.log(x);
+//    }
+//    else{
+//        console.log(y);
+//    }
+//}
+//num()
+
+var answer = document.getElementById('answer');
+var btn = document.getElementById('btn');
+var out = document.getElementById('out');
+var random = Math.floor(Math.random() * 10 + 1);
+console.log(random);
+
+btn.onclick = function () {
+    
 }
-
-else if(x < y){
-    alert('ikkinchi raqam 2 kotta');
-}
-else{
-    alert('raqamlar barvar');
-}
-
