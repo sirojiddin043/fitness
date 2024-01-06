853a78d9ab319cd0f4d59cba7da8e5c49fb9a342
sirojiddin043 <151430587+sirojiddin043@users.noreply.github.com>
2024-01-06 13:12:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -310,6 +310,47 @@
 
 
 
+// const box = document.querySelector('#box');
+// const buttons = document.querySelectorAll('button');
+// const circles = document.querySelectorAll('.circle');
+// const circle = document.querySelector('.circle');
+// const hearts = document.querySelectorAll('.heart');
+// const circleWrapper = document.querySelector('.circle__wrapper');
+
+// box.style.cssText = 'backgroundColor: red; width: 100px; height: 100px';
+// buttons[0].style.width = '100px';
+// circles[1].style.backgroundColor = 'yellow';
+// circle.style.backgroundColor = 'yellow';
+
+// // for(let i = 0; i < hearts.length; i++){
+// //     hearts[i].style.backgroundColor = 'green'
+// // }
+
+// // hearts.forEach((item) =>{
+// //     item.style.backgroundColor = 'gray'
+// // })
+
+
+// const btn = document.createElement('button');
+
+// // const text = document.createTextNode('i am text');
+
+// document.body.append(btn);
+// const myCircle = document.createElement('div');
+
+// myCircle.classList.add('circle');
+
+// circleWrapper.append(myCircle);
+// circleWrapper.before(myCircle);
+// circleWrapper.after(myCircle);
+// circles[0].after(myCircle)
+// circles[1].remove()
+// circleWrapper.append(myCircle);
+// myCircle.innerHTML = '<pre>E</pre>';
+// myCircle.textContent = 'E';
+// circleWrapper.insertAdjacentHTML('afterbegin', '<pre>E</pre>');
+
+
 
 
 
